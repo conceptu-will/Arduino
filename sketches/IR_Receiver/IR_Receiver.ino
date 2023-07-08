864f0344c9753bc9a35c384ebed891e6d990f512
Will Garnett <conceptu.will@gmail.com>
2023-07-07 22:04:50 -0400

    IR receiver improvements

--- sketches/IR_Receiver/IR_Receiver.ino
+++ sketches/IR_Receiver/IR_Receiver.ino
@@ -2,11 +2,17 @@
 
 // Testing with IRremote 4.1.2
 
-#define IR_RECEIVE_PIN 11
+#define IR_RECEIVE_PIN 2
 
 void setup() {
   Serial.begin(9600);
+
+  Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
   IrReceiver.begin(IR_RECEIVE_PIN);
+
+  Serial.print(F("Ready to receive IR signals of protocols: "));
+  printActiveIRProtocols(&Serial);
 }
 
 void loop() {
